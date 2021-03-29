7f7077ff325caba2241dd34f557f8a314b393339
Hoking2012 <81588130+Hoking2012@users.noreply.github.com>
2021-03-29 22:16:19 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+        // function onMouseOver(el) {
+        //   el.style.cssText = "background: linear-gradient(to right, #0f0c29, #302b63, #24243e);";
+        // }
+        // function onMouseOut(el) {
+        //   el.style.cssText = "background: linear-gradient(to right, #2C5364, #203A43, #0F2027);";
+        // }
+
+
+                // //НЕ РАБОТАЕТ
+                // setInterval(change1, 2000);
+                //
+                // var change1 = document.getElementById('About_me');
+                //
+                // function change1 () {
+                // document.getElementById('About_me').color = "black";
+                // }
